2c8e1c449048a9c5718129e912628b2397f05a85
samod <samuel.domin@dominanz.sk>
2021-10-15 00:42:16 +0200

    mockovane menu

--- index.php
+++ index.php
@@ -4,3 +4,75 @@ $nazovSuboru="Vitejte";
   $bc_nazov = "Domov";
   include "widgets/navbar.php";
 ?>
+<div class="container-fluid">
+    <h3>Menu na aktualny tyzden</h3>
+    <table class="table table-stripped">
+        <tbody>
+            <tr><th colspan="2" class="table-active" style="text-align: center">Pondeli</th></tr>
+            <tr>
+                <th>Polivka</th>
+                <td>Kulajda</td>
+            </tr>
+            <tr>
+                <th>Hlavni jidlo:</th>
+                <td> Kureci rezen s brambory a salatem</td>
+            </tr>
+
+            <tr><th colspan="2" class="table-active" style="text-align: center">Utery</th></tr>
+            <tr>
+                <th>Polivka</th>
+                <td>Kulajda</td>
+            </tr>
+            <tr>
+                <th>Hlavni jidlo:</th>
+                <td> Kureci rezen s brambory a salatem</td>
+            </tr>
+            <tr><th colspan="2" class="table-active" style="text-align: center">Streda</th></tr>
+            <tr>
+                <th>Polivka</th>
+                <td>Kulajda</td>
+            </tr>
+            <tr>
+                <th>Hlavni jidlo:</th>
+                <td> Kureci rezen s brambory a salatem</td>
+            </tr>
+            <tr><th colspan="2" class="table-active" style="text-align: center">Ctvrtek</th></tr>
+            <tr>
+                <th>Polivka</th>
+                <td>Kulajda</td>
+            </tr>
+            <tr>
+                <th>Hlavni jidlo:</th>
+                <td> Kureci rezen s brambory a salatem</td>
+            </tr>
+            <tr><th colspan="2" class="table-active" style="text-align: center">Patek</th></tr>
+            <tr>
+                <th>Polivka</th>
+                <td>Kulajda</td>
+            </tr>
+            <tr>
+                <th>Hlavni jidlo:</th>
+                <td> Kureci rezen s brambory a salatem</td>
+            </tr>
+            <tr><th colspan="2" class="table-active" style="text-align: center">Sobota</th></tr>
+            <tr>
+                <th>Polivka</th>
+                <td>Kulajda</td>
+            </tr>
+            <tr>
+                <th>Hlavni jidlo:</th>
+                <td> Kureci rezen s brambory a salatem</td>
+            </tr>
+            <tr><th colspan="2" class="table-active" style="text-align: center">Nedele</th></tr>
+            <tr>
+                <th>Polivka</th>
+                <td>Kulajda</td>
+            </tr>
+            <tr>
+                <th>Hlavni jidlo:</th>
+                <td> Kureci rezen s brambory a salatem</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
