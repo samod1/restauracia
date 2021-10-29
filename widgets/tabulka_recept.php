5d0ec6b84d58a63c7b63482b9568ebe593e4d079
samod <samuel.domin@dominanz.sk>
2021-10-29 16:17:05 +0200

    nefunkcne odstranovanie zaznamov

---
+++ widgets/tabulka_recept.php
@@ -0,0 +1,45 @@
+<?php
+    $conn="";
+    include "config.php";
+?>
+<table class="table table-striped">
+    <thead>
+    <tr>
+        <th>Nazov jedla</th>
+        <th>Kategoria jedla</th>
+        <th>Alergeny</th>
+        <th colspan="3">Akcia</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php
+
+    $query = "SELECT id, nazov, nazov_typu_receptu FROM restauracia.recept 
+            INNER JOIN restauracia.typ_receptu ON id_typu_receptu=typ_receptu ORDER BY id ASC ";  //uspodiadaj ASC od najmensieho po najvacsi
+    $result = mysqli_query($conn, $query); // mysqli_query - vykona prikaz
+    $pocetRiadkov = mysqli_num_rows($result);
+    if (!$result) {
+        echo "Error: Neda sa vykonat prikaz SQL: " . $query . ".<br>" . PHP_EOL;
+        exit;
+    }
+    if ($pocetRiadkov == 0) {
+        echo "Nemam co zobrazit";
+    }
+    while ($row = mysqli_fetch_assoc($result)) {
+    ?>
+    <tr>
+        <td><a id="zobrazObedy" href="detail_jedla.php?id=<?php echo $row["id"]; ?>"><?php echo $row["nazov"]?></a></td>
+        <td><?php echo $row["nazov_typu_receptu"]?></td>
+        <td><?php echo /*$row["nazov"]*/"1,2,4"?></td>
+        <td><a class="btn btn-primary" id="zobrazObedy" href="detail_jedla.php?id=<?php echo $row["id"]; ?>"><i class="fa fa-search"></i> Detail jedla</a></td>
+        <td><a class="btn btn-primary" id="Priradsuroviny" href="priradenie_surovin.php?id=<?php echo $row["id"]; ?>"><i class="fa fa-search"></i> Prirad suroviny jedla</a></td>
+        <td><a onclick="return confirm('Chcete vymazat tento recept');" class="btn btn-danger" id="zmaz" href="zoznam_receptov.php?id=<?php echo $row["id"]; ?>&zmazat=ano"><i class="fa fa-trash"></i> Odstranit recept</a></td>
+
+
+        <?php
+        }
+
+
+        ?>
+    </tbody>
+</table>
--- zoznam_receptov.php
+++ zoznam_receptov.php
@@ -6,6 +6,18 @@ include "widgets/header.php";
 
 $bc_nazov = "Prehlad receptov";
 include "widgets/navbar.php";
+
+if ($_GET["zmazat"] == "ano" && $_GET["id"] != "") {
+
+    $id = $_GET["id"];
+    $query = "DELETE FROM restauracia.recept WHERE id=?";
+    $stmt = mysqli_stmt_init($conn);
+    mysqli_stmt_prepare($stmt, $query);
+    mysqli_stmt_bind_param($stmt, "i", $id);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+}
+
 ?>
 
 <div class="container-fluid">
@@ -17,45 +29,9 @@ include "widgets/navbar.php";
             <a href="new_recept.php" class="btn btn-primary"><i class="fa fa-plus"></i> Vytvorit novy recept</a>
         </div>
     </div>
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <th>Nazov jedla</th>
-                <th>Kategoria jedla</th>
-                <th>Alergeny</th>
-                <th colspan="2">Akcia</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            $query = "SELECT id, nazov, nazov_typu_receptu FROM recept 
-            INNER JOIN typ_receptu ON id_typu_receptu=typ_receptu ORDER BY id ASC ";  //uspodiadaj ASC od najmensieho po najvacsi
-            $result = mysqli_query($conn, $query); // mysqli_query - vykona prikaz
-            $pocetRiadkov = mysqli_num_rows($result);
-            if (!$result) {
-                echo "Error: Neda sa vykonat prikaz SQL: " . $query . ".<br>" . PHP_EOL;
-                exit;
-            }
-            if ($pocetRiadkov == 0) {
-
-                echo "Nemam co zobrazit";
-
-            }
-            while ($row = mysqli_fetch_assoc($result)) {
-            ?>
-        <tr>
-            <td><a id="zobrazObedy" href="detail_jedla.php?id=<?php echo $row["id"]; ?>"><?php echo $row["nazov"]?></a></td>
-            <td><?php echo $row["nazov_typu_receptu"]?></td>
-            <td><?php echo /*$row["nazov"]*/"1,2,4"?></td>
-            <td><a class="btn btn-primary" id="zobrazObedy" href="detail_jedla.php?id=<?php echo $row["id"]; ?>"><i class="fa fa-search"></i> Detail jedla</a></td>
-            <td><a class="btn btn-primary" id="Priradsuroviny" href="priradenie_surovin.php?id=<?php echo $row["id"]; ?>"><i class="fa fa-search"></i> Prirad suroviny jedla</a></td>
-
-
-        <?php
-            }
-        ?>
-        </tbody>
-    </table>
+<?php
+include "widgets/tabulka_recept.php";
+?>
 </div>
 <?php
 mysqli_close($conn);
