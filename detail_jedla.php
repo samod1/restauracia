f3885e4843eba03f42cc0424a49b7d6b7ed9b414
samod <samuel.domin@dominanz.sk>
2021-11-06 10:37:41 +0100

    Detail receptu issue #12

--- detail_jedla.php
+++ detail_jedla.php
@@ -9,49 +9,75 @@ include "widgets/navbar.php";
 
 <?php
 if ($_GET["id"] != "") {
-    $queryNazov="SELECT nazov,postup FROM recept WHERE id=".$_GET["id"];
+    $queryNazov="SELECT nazov FROM restauracia.recept WHERE id=".$_GET["id"];
     $resultNazov = mysqli_query($conn, $queryNazov);
     while ($rowNazov = mysqli_fetch_assoc($resultNazov))
-    {
-        echo"
+    { ?>
+
 <div class='container-fluid'>
     <div class='row'>
+        <div class='col-8'>
+            <h3>Detail jedla: <?php echo $rowNazov["nazov"];} ?> </h3>
+        </div>
         <div class='col'>
-            <h3>Detail jedla: " . $rowNazov["nazov"] . "</h3>
-        <div>
-        <div class='col'>
-            <a href='editacia_receptu.php?id=".$_GET["id"]."' class='btn btn-primary'><i class='fa fa-pencil'></i> Edituj</a>
+            <a href='editacia_receptu.php?id=<?php echo $_GET["id"] ?>' class='btn btn-primary'><i class='fa fa-pencil'></i> Edituj</a>
         </div>
         <div class='col'>
             <a href='#' class='btn btn-danger 'onclick='return confirm('Naozaj chces vykonat tieto zmeny ?');'><i class='fa fa-trash'></i> Zmazat</a>
         </div>
      </div>
-            <h4>Suroviny prepocet na jednu osobu</h4>
-            <table class='table tbl-stripped'>
-                <thead class='table thead-light'>
-                    <tr>
-                        <th>Surovina</th>
-                        <th>Mnozstvo</th>
-                    </tr>
-                </thead> 
-                <tbody>
-                    <tr>
-                        <td>Kuracie maso</td>
-                        <td>1kg</td>
-                    </tr>
-                    <tr>
-                        <td>Cibula</td>
-                        <td>0,250kg</td>
-                    </tr>
-                </tbody>       
-            </table>
+
+    <h4>Suroviny prepocet na jednu osobu</h4>
+    <table class='table tbl-stripped'>
+        <thead class='table thead-light'>
+        <tr>
+            <th>Surovina</th>
+            <th>Mnozstvo</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+        $querySuroviny="SELECT nazov_suroviny ,mnozstvo, enum_jednotka.skratka FROM restauracia.suroviny_k_receptu 
+        INNER JOIN restauracia.tbl_suroviny ON suroviny_k_receptu.id_sur = tbl_suroviny.id_suroviny
+        INNER JOIN restauracia.enum_jednotka ON suroviny_k_receptu.jednotka = enum_jednotka.id_jednotky
+        WHERE id_rec =".$_GET["id"];
+        $resultSuroviny= mysqli_query($conn,$querySuroviny);
+        $pocetRiadkov = mysqli_num_rows($resultSuroviny);
+
+        if (!$resultSuroviny) {
+            echo "Error: Neda sa vykonat prikaz SQL: " . $querySuroviny . ".<br>" . PHP_EOL;
+            exit;
+        }
+        if ($pocetRiadkov == 0) {
+            echo "Nemam co zobrazit";
+        }
+        while ($rowSuroviny = mysqli_fetch_assoc($resultSuroviny)) {
+            ?>
+            <tr>
+                <td><?php echo $rowSuroviny["nazov_suroviny"]; ?></td>
+                <td><?php echo $rowSuroviny["mnozstvo"]." ".$rowSuroviny["skratka"]; ?></td>
+            </tr>
+        <?php } ?>
+        </tbody>
+    </table>
+
             <h4>Postup</h4>
-            <p>".$rowNazov["postup"]."</p>
+            <?php
+
+            $queryNazov="SELECT postup FROM restauracia.recept WHERE id=".$_GET["id"];
+            $resultNazov = mysqli_query($conn, $queryNazov);
+            while ($rowNazov = mysqli_fetch_assoc($resultNazov))
+            { ?>
+                <p> <?php echo $rowNazov["postup"]; ?> </p>
+    <?php } ?>
             </div>
-</div>";
+
+
+
+<?php
     }
-}
 ?>
+
 </div>
 <?php
 mysqli_close($conn);
