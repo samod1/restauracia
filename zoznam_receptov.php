acf05f4de172744f275dceec851eadf4fcb59cd8
samod <samuel.domin@dominanz.sk>
2021-11-08 09:13:17 +0100

    odstranenie spracovania mazania

--- zoznam_receptov.php
+++ zoznam_receptov.php
@@ -7,17 +7,6 @@ include "widgets/header.php";
 $bc_nazov = "Prehlad receptov";
 include "widgets/navbar.php";
 
-if ($_GET["zmazat"] == "ano" && $_GET["id"] != "") {
-
-    $id = $_GET["id"];
-    $query = "DELETE FROM restauracia.recept WHERE id=?";
-    $stmt = mysqli_stmt_init($conn);
-    mysqli_stmt_prepare($stmt, $query);
-    mysqli_stmt_bind_param($stmt, "i", $id);
-    mysqli_stmt_execute($stmt);
-    mysqli_stmt_close($stmt);
-}
-
 ?>
 
 <div class="container-fluid">
