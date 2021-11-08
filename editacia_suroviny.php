7f56bce81c0e23df29654e9062123a49ed34eaea
samod <samuel.domin@dominanz.sk>
2021-11-08 09:11:32 +0100

    editacia suroviny issue #9

--- editacia_suroviny.php
+++ editacia_suroviny.php
@@ -6,7 +6,7 @@ include "widgets/header.php";
 include "config.php";
 include "widgets/navbar.php";
 
-if ($_GET["id"] !=="")
+if ($_GET["id"] !=="" && $_GET["edituj"]=="ano")
 {
     $idSuroviny = $_GET["id"];
     $query="SELECT nazov_suroviny,nazov_kategorie,id_kategorie FROM restauracia.tbl_suroviny 
@@ -19,39 +19,24 @@ if ($_GET["id"] !=="")
         echo "Error: Neda sa vykonat prikaz SQL: " . $query . ".<br>" . PHP_EOL;
         exit;
     }
-    ?>
-    <h3>Editacia suroviny</h3>
-    <br>
-    <form method="post" class="form-group">
-        <?php
-        while ($row = mysqli_fetch_assoc($result)) { ?>
-            <label>Nazov suroviny</label>
-            <input name="surovina" type="text" class="form-control" value="<?php echo $row["nazov_suroviny"]?>">
-            <label>Kategoria suroviny</label>
-            <select class="form-control" name="kategoria">
-                <option value="<?php echo $row["id_kategorie"] ?>"><?php echo $row["nazov_kategorie"] ?></option>
-            </select>
+    while ($row = mysqli_fetch_assoc($result)) {
+        $nazovSurovinyp = $row["nazov_suroviny"];
+    }
 
-            <?php
-        }
-        ?>
-        <br>
-        <input type="submit" class="btn btn-primary btn-lg btn-block">
-        <input type="hidden" name="edit" value="yes">
-    </form>
-    <?php
+    include "spracovania/Edit_form.php";
+}
     if ($_POST["edit"]=="yes")
     {
         $queryEdit="UPDATE restauracia.tbl_suroviny SET nazov_suroviny='?' WHERE id_suroviny=?";
         $stmt = mysqli_stmt_init($conn);
-        mysqli_stmt_prepare($stmt,$query);
+        mysqli_stmt_prepare($stmt,$queryEdit);
         mysqli_stmt_bind_param($stmt,"si",$_POST["surovina"],$idSuroviny);
         mysqli_stmt_execute($stmt);
         mysqli_stmt_close($stmt);
         mysqli_commit($conn);
         header("Location: tbl_suroviny.php");
     }
-}
+
 mysqli_close($conn);
 include "widgets/footer.php";
 ?>
---
+++ spracovania/Edit_form.php
@@ -0,0 +1,9 @@
+<h3>Editacia suroviny</h3>
+<br>
+<form action="../editacia_suroviny.php" method="post" class="form-group">
+    <label>Nazov suroviny</label>
+    <input name="surovina" type="text" class="form-control" value="<?php echo $nazovSurovinyp?>">
+    <br>
+    <input type="submit" class="btn btn-primary btn-lg btn-block">
+    <input type="hidden" name="edit" value="yes">
+</form>
