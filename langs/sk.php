c4185d74b552cabf1afc31a68f0303417d2c4038
samuel.domin@dominanz.sk <samuel.domin@dominanz.sk>
2021-11-27 23:35:18 +0100

    Doplnanie vyrazov pre slovensku mutaciu stranky

--- langs/sk.php
+++ langs/sk.php
@@ -1,11 +1,20 @@
 <?php
 //Slovensky jazyk
 
-$lang= array("TITLE" => 'Reštaurácia',
+$lang= array(
+    "TITLE" => 'Reštaurácia',
     "HOME" => 'Domov',
     "INGREDIENTS" => "Suroviny",
     "RECIEPES" => "Recepty",
+    "MENUS" => "Jedálne lístky",
+    "STORAGE" => "Sklad",
     "lang_cz" => "Česky",
-    "lang_sk" => "Slovensky"
+    "lang_sk" => "Slovensky",
+    "STATISTICS" => "Štatistiky",
+    "GUEST_COUNT" => "Počet hostí tento týždeň",
+    "MENU_COUNT" => "Počet vytvorených menu",
+    "RECIEPE_COUNT" => "Počet vytvorených receptov",
+    "DAY" => "Deň",
+    "TODAYS_MENU" => "Dnešné menu"
 );
 ?>
