25c99f7274d84599dc3e852c54d6c7f87a1e0230
samuel.domin@dominanz.sk <samuel.domin@dominanz.sk>
2021-11-28 11:52:11 +0100

    uprava konfiguracnych suborov v projekte

--- config.php
+++ config.php
@@ -1,25 +1,14 @@
 <?php
-session_start();
+    session_start();
 
-if (!isset($_SESSION['lang']))
-    $_SESSION['lang'] = "sk";
-else if (isset($_GET['lang']) && $_SESSION['lang'] != $_GET['lang'] && !empty($_GET['lang'])) {
-    if ($_GET['lang'] == "sk")
+    if (!isset($_SESSION['lang']))
         $_SESSION['lang'] = "sk";
-    else if ($_GET['lang'] == "cz")
-        $_SESSION['lang'] = "cz";
-}
+    else if (isset($_GET['lang']) && $_SESSION['lang'] != $_GET['lang'] && !empty($_GET['lang'])) {
+        if ($_GET['lang'] == "sk")
+            $_SESSION['lang'] = "sk";
+        else if ($_GET['lang'] == "cz")
+            $_SESSION['lang'] = "cz";
+    }
 
-require_once "langs/" . $_SESSION['lang'] . ".php";
-$server = "db.dw082.nameserver.sk";
-$user = "demorest_1";
-$pass = "Test1234";
-$db = "demorest";
-
-$conn = mysqli_connect($server,$user,$pass,$db);
-
-/*if (!$conn) {
-    die("Spojenie neuspesne: " . mysqli_connect_error());
-}
-echo "Pripojenie prebehlo uspesne.";*/
+    require_once "langs/" . $_SESSION['lang'] . ".php";
 ?>
