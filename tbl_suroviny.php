02820c5f5c3296d5c23dccbda2dbcd5564d1cf52
samuel.domin@dominanz.sk <samuel.domin@dominanz.sk>
2021-11-28 20:20:02 +0100

    Implementacia okna so surovinami #35

--- tbl_suroviny.php
+++ tbl_suroviny.php
@@ -1,48 +1,54 @@
 <?php
 include "config.php";
-$nazovSuboru="Prehlad surovin";
+
+$nazovSuboru= $lang["suroviny"];
 include "widgets/header.php";
+
 $bc_nazov= $lang["suroviny"];
 include "widgets/navbar.php";
+
 $conn = "";
 include "configDb.php";
 ?>
 <div class="container-fluid">
     <h3><?php echo $lang["suroviny"];?></h3>
 <div class="row">
-    <form class="form-group" method="post">
-        <div class="col-6">
+    <div class="col"><a href="vkladanie_surovin.php" class="btn btn-primary btn-lg"><?php echo $lang["novaSur"]?></a> </div>
+</div>
+    <form method="get" class="form-group">
+        <div class="row">
             <label><?php echo $lang["kat_suroviny"];?></label>
-            <select name="kategoria" class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
-                <?php
-                    $query="SELECT id_kategorie,nazov_kategorie FROM enum_kategoria_suroviny order by nazov_kategorie ASC";
+            <div class="col-8">
+                <select name="kategoria" class="form-control form-control-lg">
+                    <?php
+                    $query="SELECT id_kategorie,nazov_kategorie FROM enum_kategoria_suroviny ORDER BY nazov_kategorie ASC";
                     $result = mysqli_query($conn,$query);
                     while($row=mysqli_fetch_assoc($result))
                     {
-                 ?>
-                <option value="<?php echo $row["id_kategorie"];?>"><?php echo $row["nazov_kategorie"];?></option>
-                <?php
+                        ?>
+                        <option value="<?php echo $row["id_kategorie"];?>"><?php echo $row["nazov_kategorie"];?></option>
+                        <?php
                     }
-                ?>
-            </select>
-        </div>
-        <div class="col-2">
-            <input type="submit" value="<?php echo $lang["hladaj"];?>" class="btn btn-primary btn-lg">
-        </div>
+                    ?>
+                </select>
+            </div>
+            <div class="col-4">
+                <input type="submit" value="<?php echo $lang["hladaj"];?>" class="btn btn-primary btn-lg">
+            </div>
     </form>
+
 </div>
 
 <?php
-if ($_POST["kategoria"]!="")
+if ($_GET["kategoria"] != "")
 {
-    $querySur = "SELECT nazov_suroviny,Mnozstvo,jednotka FROM tbl_suroviny 
-    INNER JOIN enum_jednotka ej on tbl_suroviny.jednotka = ej.id_jednotky ORDER BY nazov_suroviny ASC";
-    $resultSur = mysqli_query($conn,$querySur);
-    $pocetRiadkovSur = mysqli_num_rows($resultSur);
+    $queryKat = "SELECT nazov_suroviny FROM tbl_suroviny";
+    $resultKat = mysqli_query($conn,$queryKat);
+    $pocetRiadkovSur = mysqli_num_rows($resultKat);
+
     if ($pocetRiadkovSur == 0)
     {
-        echo "
-        <div class='alert alert-warning alert-dismissible fade show' role='alert'>
+        echo "<div class='alert alert-warning alert-dismissible fade show' role='alert'>
             <svg class='bi flex-shrink-0 me-2' width='24' height='24' role='img' aria-label='Danger:'><use xlink:href='#exclamation-triangle-fill'/></svg>
             <strong>".$lang["noRecords"]."</strong>
             <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
@@ -51,8 +57,7 @@ if ($_POST["kategoria"]!="")
 
     else
     {
-        $querySur = "SELECT nazov_suroviny,Mnozstvo,jednotka FROM tbl_suroviny 
-        INNER JOIN enum_jednotka ej on tbl_suroviny.jednotka = ej.id_jednotky  WHERE kategoria_suroviny=".$_POST["kategoria"]." ORDER BY nazov_suroviny ASC";
+        $querySur = "SELECT nazov_suroviny,Mnozstvo,skratka FROM tbl_suroviny INNER JOIN enum_jednotka ej on tbl_suroviny.jednotka = ej.id_jednotky WHERE kategoria_suroviny =". $_GET["kategoria"];
         $resultSur = mysqli_query($conn,$querySur);
         ?>
         <table class="table table-stripped">
@@ -61,18 +66,47 @@ if ($_POST["kategoria"]!="")
                 <th><?php echo $lang["mnozstvo"]; ?></th>
             </thead>
 
+
             <tbody>
             <?php
-        while ($row=mysqli_fetch_assoc($resultSur))
-        { ?>
-            <td><?php echo $row["nazov_suroviny"]?></td>
-            <td><?php echo $row["mnozstvo"]." ".$row["jednotka"]?></td>
+            while ($rowSur = mysqli_fetch_assoc($resultSur))
+            { ?>
+                <td><?php echo $rowSur["nazov_suroviny"];?></td>
+                <td><?php echo $rowSur["Mnozstvo"]." ".$rowSur["skratka"];?></td>
        <?php } ?>
             </tbody>
         </table>
             <?php
     }
 }
+
+else
+{
+    $querySur = "SELECT nazov_suroviny,Mnozstvo,skratka FROM tbl_suroviny INNER JOIN enum_jednotka ej on tbl_suroviny.jednotka = ej.id_jednotky";
+    $resultSur = mysqli_query($conn,$querySur);
+    ?>
+    <table class="table table-stripped">
+        <thead>
+            <th><?php echo $lang["nazov"]; ?></th>
+            <th><?php echo $lang["mnozstvo"]; ?></th>
+        </thead>
+
+
+        <tbody>
+        <?php
+        while ($rowSur = mysqli_fetch_assoc($resultSur))
+        { ?>
+                <tr>
+                    <td><?php echo $rowSur["nazov_suroviny"];?></td>
+                    <td><?php echo $rowSur["Mnozstvo"]." ".$rowSur["skratka"];?></td>
+                </tr>
+        <?php } ?>
+        </tbody>
+    </table>
+
+    <?php
+}
+
 ?>
 
 </div>
