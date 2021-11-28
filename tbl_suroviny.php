88556b5fb68f59705140cb3d2857f5d45456d6ea
samuel.domin@dominanz.sk <samuel.domin@dominanz.sk>
2021-11-28 23:28:28 +0100

    Implementacia modalneho okna #35

--- tbl_suroviny.php
+++ tbl_suroviny.php
@@ -20,6 +20,7 @@ include "configDb.php";
             <label><?php echo $lang["kat_suroviny"];?></label>
             <div class="col-8">
                 <select name="kategoria" class="form-control form-control-lg">
+                    <option value="all"><?php echo $lang["all"]?></option>
                     <?php
                     $query="SELECT id_kategorie,nazov_kategorie FROM enum_kategoria_suroviny ORDER BY nazov_kategorie ASC";
                     $result = mysqli_query($conn,$query);
@@ -32,15 +33,19 @@ include "configDb.php";
                     ?>
                 </select>
             </div>
-            <div class="col-4">
+            <div class="col-2">
                 <input type="submit" value="<?php echo $lang["hladaj"];?>" class="btn btn-primary btn-lg">
             </div>
+            <div class="col-2">
+                <input type="reset" class="btn btn-secondary btn-lg">
+            </div>
+        </div>
     </form>
 
-</div>
+
 
 <?php
-if ($_GET["kategoria"] != "")
+if ($_GET["kategoria"] != "" && $_GET["kategoria"]!="all")
 {
     $queryKat = "SELECT nazov_suroviny FROM tbl_suroviny";
     $resultKat = mysqli_query($conn,$queryKat);
@@ -57,7 +62,7 @@ if ($_GET["kategoria"] != "")
 
     else
     {
-        $querySur = "SELECT nazov_suroviny,Mnozstvo,skratka FROM tbl_suroviny INNER JOIN enum_jednotka ej on tbl_suroviny.jednotka = ej.id_jednotky WHERE kategoria_suroviny =". $_GET["kategoria"];
+        $querySur = "SELECT id_suroviny,nazov_suroviny,Mnozstvo,skratka FROM tbl_suroviny INNER JOIN enum_jednotka ej on tbl_suroviny.jednotka = ej.id_jednotky WHERE kategoria_suroviny =". $_GET["kategoria"];
         $resultSur = mysqli_query($conn,$querySur);
         ?>
         <table class="table table-stripped">
@@ -71,10 +76,20 @@ if ($_GET["kategoria"] != "")
             <?php
             while ($rowSur = mysqli_fetch_assoc($resultSur))
             { ?>
-                <td><?php echo $rowSur["nazov_suroviny"];?></td>
+                <td><a data-id="@idSur" data-toggle="modal" data-target="#suroviny"><?php echo $rowSur["nazov_suroviny"];?></a></td>
                 <td><?php echo $rowSur["Mnozstvo"]." ".$rowSur["skratka"];?></td>
+
+                <script>
+                    $(document).ready(function () {
+                        $(".open-suroviny").click(function () {
+                            $('#bookId').val($(this).data(<?php echo $rowSur["id_suroviny"]?>));
+
+                        });
+                    });
+                </script>
        <?php } ?>
             </tbody>
+
         </table>
             <?php
     }
@@ -104,11 +119,28 @@ else
         </tbody>
     </table>
 
-    <?php
+<?php
 }
-
 ?>
-
+    <div class="modal fade" id="suroviny" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel"><?php echo $lang["detailSur"];?></h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <h6 id="idSur"></h6>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal"><?php echo $lang["close"]?></button>
+                    <button type="button" class="btn btn-primary"><?php echo $lang["save"]?></button>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 <?php
     include_once "widgets/footer.php";
