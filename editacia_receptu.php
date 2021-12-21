a790536825372a455ba13583ed7e19317dd6c888
samuel.domin@dominanz.sk <samuel.domin@dominanz.sk>
2021-12-21 18:48:33 +0100

    editacia receptu, pripravit nasadenie na restauracia

--- editacia_receptu.php
+++ editacia_receptu.php
@@ -1,20 +1,49 @@
 <?php
+$conn="";
+include "config.php";
+include "configDb.php";
 $nazovSuboru="Editacia receptu";
 include "widgets/header.php";
 
 $bc_nazov="Editacia receptu";
 include "widgets/navbar.php";
+
+if($_GET["id"] !="" && $_GET["edituj"]="yes")
+{
 ?>
 <div class="container-fluid">
     <h3>Editacia receptu</h3>
-
-    <form class="form-group">
+    <form class="form-group" method="post" action="spracovanie_editacie.php">
+        <input name="idReceptu" type="hidden" value="<?php echo $_GET["id"];?>">
+        <?php
+            $query="SELECT nazov_receptu,postup_receptu,cena_jedla,alergeny FROM demorest.tbl_recept WHERE id_receptu=".$_GET["id"];
+            $result = mysqli_query($conn,$query);
+            while ($rowRec = mysqli_fetch_assoc($result))
+            {
+        ?>
         <label>Nazov receptu</label>
-        <input class="form-control form-control-lg" type="text">
+        <input name="nazovReceptu" class="form-control form-control-lg" type="text" value="<?php echo $rowRec["nazov_receptu"];?>">
         <br>
+
+        <label><?php echo $lang["postup"];?></label>
+        <textarea name="postupReceptu" class="form-control" rows="10" value="<?php echo $rowRec["postup_receptu"];?>"><?php echo $rowRec["postup_receptu"];?></textarea>
+        <br>
+
+        <label>Alergeny</label>
+        <input name="alergeny" class="form-control form-control-lg" type="text" value="<?php echo $rowRec["alergeny"];?>"><br>
+
+                <br>
+        <label>Cena jedla</label>
+        <input name="cenaJedla" class="form-control form-control-lg" type="text" value="<?php echo $rowRec["cena_jedla"];?>">
+        <br>
+            <?php }?>
         <input type="submit" class="btn btn-primary btn-lg btn-block" value="Ulozit">
+        <input type="hidden" name="edit" value="yes">
+
     </form>
 </div>
 <?php
+
+}
 include "widgets/footer.php";
 ?>
---
+++ spracovanie_editacie.php
@@ -0,0 +1,27 @@
+<?php
+$conn = "";
+include "configDb.php";
+if ($_POST["edit"]=="yes")
+{
+    //definovanie premennych
+    $idReceptu  = $_POST["idReceptu"];
+    $nazovReceptu = $_POST["nazovReceptu"];
+    $postupReceptu = $_POST["postupReceptu"];
+    $alergeny = $_POST["alergeny"];
+    $cenaJedla = $_POST["cenaJedla"];
+
+    //UPDATE querina
+
+    $query = "UPDATE tbl_recept SET nazov_receptu=?,postup_receptu=?,alergeny=?,cena_jedla=? WHERE id_receptu=?";
+    $stmt = mysqli_stmt_init($conn);
+    mysqli_stmt_prepare($stmt, $query);
+    mysqli_stmt_bind_param($stmt, 'sssii', $nazovReceptu, $postupReceptu,$alergeny,$cenaJedla,$idReceptu);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+    header("Location: zoznam_receptov.php");
+
+    mysqli_close($conn);
+
+
+}
+
