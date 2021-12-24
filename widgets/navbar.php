9fa97a5e79bdb53ab99be56a648dbca6772a362e
Samuel Domin <samuel.domin@dominanz.sk>
2021-12-24 12:04:27 +0100

    Prerobit menu a breadcrumbs. #39

--- widgets/navbar.php
+++ widgets/navbar.php
@@ -2,39 +2,25 @@
     <div class="jumbotron-fluid">
         <h1 class="display-4" style="text-align: center"><?php echo $lang["TITLE"]; ?></h1>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <ul class="navbar nav">
-                <li class="nav-item"><a class="nav-link" href="../index.php"><?php echo $lang["HOME"];?></a></li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <?php echo $lang["INGREDIENTS"];?>
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="../tbl_suroviny.php">Zoznam surovín</a>
-                        <a class="dropdown-item" href="../vkladanie_surovin.php">Vkladanie surovín</a>
-
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Recepty</a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="../zoznam_receptov.php">Zoznam receptov</a>
-                        <a class="dropdown-item" href="../new_recept.php">Vytvoriť nový recept</a>
-                    </div>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="../jedalny_listok.php" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <?php echo $lang["MENUS"];?></a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="../jedalny_listok.php">Jedálne lístky</a>
-                        <a class="dropdown-item" href="../nove_menu.php">Vytvoriť nové menu</a>
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="../sklad.php"><?php echo $lang["STORAGE"];?></a>
-                </li>
-
-
-            </ul>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="../index.php">Domov</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../tbl_suroviny.php">Suroviny</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../zoznam_receptov.php">Recepty</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../jedalny_listok.php">Jedalne listky</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../sklad.php">Sklad</a>
+                    </li>
+                </ul>
+            </div>
         </nav>
         <nav aria-label="breadcrumb">
             <ol class="breadcrumb">
