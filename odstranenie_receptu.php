538ac731249ddc9890fa2a9a85b48523369bc3b2
Samuel Domin <samuel.domin@dominanz.sk>
2021-12-29 21:31:28 +0100

    Zmazavanie receptov
    funkcne

--- odstranenie_receptu.php
+++ odstranenie_receptu.php
@@ -4,20 +4,30 @@ include "configDb.php";
 
 if ($_GET["zmazat"] == "ano" && $_GET["id"] != "") {
 
-    $querySuroviny = "DELETE FROM tbl_suroviny_k_receptu WHERE id_rec=?";
-    $stmtSuroviny = mysqli_stmt_init($conn);
-    mysqli_stmt_prepare($stmtSuroviny, $querySuroviny);
-    mysqli_stmt_bind_param($stmtSuroviny, "i", $id);
-    mysqli_stmt_execute($stmtSuroviny);
-    mysqli_stmt_close($stmtSuroviny);
+    $query="SELECT id_rec_sur FROM tbl_suroviny_k_receptu WHERE id_rec=".$_GET["id"];
+    $result = mysqli_query($conn,$query);
+    $pocetRiadkov = mysqli_num_rows($result);
 
-    $id = $_GET["id"];
-    $query = "DELETE FROM tbl_recept WHERE id_receptu=?";
-    $stmt = mysqli_stmt_init($conn);
-    mysqli_stmt_prepare($stmt, $query);
-    mysqli_stmt_bind_param($stmt, "i", $id);
-    mysqli_stmt_execute($stmt);
-    mysqli_stmt_close($stmt);
+    if($pocetRiadkov == 0)
+    {
+        $query = "DELETE FROM tbl_recept WHERE id_receptu=". $_GET["id"];
+        $stmt = mysqli_stmt_init($conn);
+        mysqli_stmt_prepare($stmt, $query);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_close($stmt);
+        header('Refresh:3; url=zoznam_receptov.php', true, 200);
     }
+
+    else
+    {
+        while ($row= mysqli_fetch_assoc($result))
+        {
+            $querySuroviny = "DELETE FROM tbl_suroviny_k_receptu WHERE id_rec=".$_GET["id"];
+            $stmtSuroviny = mysqli_stmt_init($conn);
+            mysqli_stmt_prepare($stmtSuroviny, $querySuroviny);
+            mysqli_stmt_execute($stmtSuroviny);
+            mysqli_stmt_close($stmtSuroviny);
+        }
+    }
+}
 mysqli_close($conn);
-header('Location: zoznam_receptov.php');
