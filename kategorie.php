069e8ef7c5c846d8573f864558dfbec8b67a7c10
Samuel Domin <samuel.domin@dominanz.sk>
2021-12-30 23:47:09 +0100

    Kategorie
    recepty edit mazanie citanie

--- kategorie.php
+++ kategorie.php
@@ -112,7 +112,93 @@ include "widgets/navbar.php";
         </div>
 
         <div class="tab-pane active" id="recepty" role="tabpanel" aria-labelledby="recepty">
+            <div class="tab-pane active" id="suroviny" role="tabpanel" aria-labelledby="suroviny">
+                <?php
+                $query = "SELECT id_typu_receptu, nazov_typu_receptu FROM enum_typ_receptu";
+                $result = mysqli_query($conn,$query);
+                $pocetRiadkov = mysqli_num_rows($result);
 
+                if ($pocetRiadkov != 0)
+                {
+                    ?>
+                    <table class="table table-striped">
+                        <thead>
+                        <tr>
+                            <th>Nazov kategorie</th>
+                            <th>Pocet surovin v kategorii</th>
+                            <th colspan="2">Akcia</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php
+
+                        while ($row = mysqli_fetch_assoc($result))
+                        {
+                        $querySur = "SELECT nazov_receptu FROM tbl_recept WHERE typ_receptu= ".$row["id_typu_receptu"];
+                        $resultSur = mysqli_query($conn,$querySur);
+                        $pocetSurovin = mysqli_num_rows($resultSur);
+                        ?>
+                        <tr>
+                            <td><?php echo $row["nazov_typu_receptu"];?></td>
+                            <td><?php echo $pocetSurovin; ?></td>
+                            <td><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#message<?php echo $row['id_typu_receptu']?>">Editacia kategorie</button></td>
+                            <td><a href="spracovanie/mazanie_kategorii.php?id=<?php echo $row["id_typu_receptu"]?>&zmazat=recept" class="btn btn-danger">Zmazat</a></td>
+
+                            <!-- Modal -->
+                            <div class="modal fade" id="message<?php echo $row["id_typu_receptu"];?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel">Editacia kategorie <?php echo $row["nazov_typu_receptu"]?></h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <p>Formular editacie</p>
+                                            <form method="post" class="form-group">
+                                                <input type="hidden" name="id" value="<?php echo $row["id_typu_receptu"];?>">
+                                                <label>Nazov kategorie</label>
+                                                <input class="form-control form-control-lg" type="text" name="kategoriaRec" value="<?php echo $row["nazov_typu_receptu"];?>">
+                                                <br>
+                                                <input class="btn btn-primary btn-lg btn-block" type="submit" name="sendRec" value="Zmenit udaje">
+                                            </form>
+                                            <?php
+                                            if (isset($_POST["sendRec"]))
+                                            {
+                                                $queryEdit = "UPDATE enum_typ_receptu SET nazov_typu_receptu = ? WHERE id_typu_receptu = ? ";
+                                                $stmt = mysqli_stmt_init($conn);
+                                                mysqli_stmt_prepare($stmt,$queryEdit);
+                                                mysqli_stmt_bind_param($stmt, 'si', $_POST["kategoriaRec"],$_POST["id"]);
+                                                mysqli_stmt_execute($stmt);
+                                                mysqli_stmt_close($stmt);
+                                                mysqli_commit($conn);
+                                                header("Refresh:0; url:kategorie.php");
+                                            }
+                                            ?>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Zavriet</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <?php
+                            }
+                            ?>
+                        </tbody>
+                    </table>
+                    <?php
+                }
+
+                else
+                {
+                    echo "<div class='alert alert-warning' role='alert'>
+                    <strong>V databaze sa nenachadzaju ziadne zaznamy</strong>
+                    </div>";
+                }
+
+                ?>
         </div>
     </div>
 </div>
--- spracovanie/mazanie_kategorii.php
+++ spracovanie/mazanie_kategorii.php
@@ -37,3 +37,40 @@ if ($_GET["zmazat"] == "ano" && $_GET["id"]!="")
     }
 
 }
+
+if ($_GET["zmazat"] == "recept" && $_GET["id"]!="")
+{
+    $query="SELECT nazov_receptu FROM tbl_recept WHERE typ_receptu=".$_GET["id"];
+    $result = mysqli_query($conn,$query);
+    $pocetRiadkov = mysqli_num_rows($result);
+
+    if ($pocetRiadkov == 0)
+    {
+        $queryDel = "DELETE FROM enum_typ_receptu WHERE id_typu_receptu=?";
+        $stmt = mysqli_stmt_init($conn);
+        mysqli_stmt_prepare($stmt,$queryDel);
+        mysqli_stmt_bind_param($stmt, 'i', $_GET["id"]);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_close($stmt);
+        mysqli_commit($conn);
+        header("Refresh:3; url:../kategorie.php",true, 200);
+    }
+
+    else
+    {
+        $nazovSuboru = "Vymazanie neuspesne!";
+        ?>
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <h4 class="alert-heading">Chyba!</h4>
+            <strong>Vami zvolena kategoria sa neda vymazat z dovodu:</strong> V kategorii sa nachadzaju uz recepty.
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <?php
+        header("Refresh:3; url:../kategorie.php",true, 400);
+    }
+
+}
+
+?>
