02da01b2958d3937580c2044fed84bbd3f43b500
Samuel Domin <samuel.domin@dominanz.sk>
2021-12-30 19:21:49 +0100

    Kategorie

--- kategorie.php
+++ kategorie.php
@@ -1,2 +1,20 @@
 <?php
 $stranka = "kategorie";
+include "config.php";
+$conn ="";
+include "configDb.php";
+$nazovSuboru="Kategorie";
+include "widgets/header.php";
+$bc_nazov="Kategorie";
+include "widgets/navbar.php";
+?>
+
+<h3>Kategorie</h3>
+<ul class="nav nav-tabs">
+    <li class="nav-item">
+        <a class="nav-link active" href="#">Active</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+    </li>
+</ul>
