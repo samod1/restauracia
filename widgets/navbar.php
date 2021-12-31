d7c15ed063ac8425b95fff2b53bd55c4f2813355
Samuel Domin <samuel.domin@dominanz.sk>
2021-12-31 13:23:57 +0100

    Z navbaru odstranene nedokoncene komponenty

--- widgets/navbar.php
+++ widgets/navbar.php
@@ -17,11 +17,11 @@
                     <li class="nav-item <?php if ($stranka == "kategorie") {echo " active";} ?>">
                         <a class="nav-link" href="../kategorie.php">Kategorie</a>
                     </li>
-                    <li class="nav-item <?php if ($stranka == "listky") {echo " active";} ?>">
+                  <!--  <li class="nav-item <?php if ($stranka == "listky") {echo " active";} ?>">
                         <a class="nav-link" href="../jedalny_listok.php">Jedalne listky</a>
                     </li>
                     <li class="nav-item <?php if ($stranka == "sklad") {echo " active";} ?>">
-                        <a class="nav-link" href="../sklad.php">Sklad</a>
+                        <a class="nav-link" href="../sklad.php">Sklad</a> -->
                     </li>
                 </ul>
             </div>
