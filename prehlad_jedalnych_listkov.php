a3edda8df210f55c1d951c2c2df7e752c2f34d2b
Samuel Domin <samuel.domin@dominanz.sk>
2022-02-14 14:54:02 +0100

    uprava v prehlade vytvorenych jedalnych listkov
    premenovanie suboru

--- prehlad_jedalnych_listkov.php
+++ prehlad_jedalnych_listkov.php
@@ -1,24 +1,25 @@
 <?php
 include "config.php";
 $stranka="listky";
-$nazovSuboru="Jidelny listek";
+$nazovSuboru="Prehľad jedálnych lístkov";
 include "widgets/header.php";
-$bc_nazov="Jidelny listek";
+$bc_nazov="Jedalne listky";
 include "widgets/navbar.php";
 ?>
 <div class="container-fluid">
     <div class="row">
-        <h3>Jidelny listek</h3>
-        <div class="col-4">
-            <a href="nove_menu.php" class="btn btn-primary"><i class="fa fa-plus"></i> Vytvorit novy jedalny listok</a>
+        <h3>Prehľad jedálnych lístkov</h3>
+        <div class="col-12">
+            <a href="nove_menu.php" class="btn btn-primary btn-lg btn-block">Vytvorit novy jedalny listok</a>
+
         </div>
     </div>
-    <h4>Seznam aktualnich jidelnych listku</h4>
+    <br>
     <?php
         $conn="";
         include "configDb.php";
 
-        $query = "SELECT id_menu, datum_od, datum_do FROM tbl_menu ORDER BY id_menu ASC ";  //uspodiadaj ASC od najmensieho po najvacsi
+        $query = "SELECT id_menu, datum_od, datum_do, pocet_hosti FROM tbl_menu ORDER BY id_menu ASC ";  //uspodiadaj ASC od najmensieho po najvacsi
         $result = mysqli_query($conn, $query); // mysqli_query - vykona prikaz
         $pocetRiadkov = mysqli_num_rows($result);
         if (!$result) {
@@ -37,8 +38,8 @@ include "widgets/navbar.php";
         <thead>
             <tr>
                 <th colspan="2">Datum</th>
+                <th colspan="1">Pocet hosti</th>
                 <th colspan="1">Akcia</th>
-                <th colspan="1">Priradit jedla k menu</th>
             </tr>
         </thead>
         <tbody>
@@ -49,8 +50,8 @@ include "widgets/navbar.php";
         <tr>
             <td><?php echo $row["datum_od"] ?></td>
             <td><?php echo $row["datum_do"] ?></td>
+            <td><?php echo $row["pocet_hosti"]?></td>
             <td><a class="btn btn-primary" href="detail_jedalneho_listku.php?menu=<?php echo $row["id_menu"] ?>"><i class="fa fa-search"></i> Detail menu</a></td>
-            <td><a class="btn btn-primary" href="priradenie_jedal.php?menu=<?php echo $row["id_menu"] ?>"><i class="fa fa-arrow-right"></i> Priradit jedla</a></td>
         </tr>
         <?php
         }
